13376e6560d150ce884550ffb96bbe5d78be43ad
Oscar Anyiam <71092412+richieoscar@users.noreply.github.com>
2022-03-21 21:14:16 +0100

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,24 @@
+def buildApp(){
+    echo 'building application....'
+}
+
+def cleanApp(){
+    echo 'Cleaning code'
+    sh 'mvn clean'
+}
+
+def testApp(){
+     echo 'executing Tests'
+    sh 'mvn test'
+}
+
+def packageApp(){
+    echo 'Packaging Application....'
+    sh 'mvn pacakage'
+}
+def deployApp(){
+    echo 'Deploying Application....'
+    echo "Deploying version ${params.VERSION}"
+}
+
+return this
