67a651abedabfe39cf1c46ba47804f170b9cce0f
Oscar Anyiam <71092412+richieoscar@users.noreply.github.com>
2022-03-21 23:28:45 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -14,11 +14,25 @@ def testApp(){
 
 def packageApp(){
     echo 'Packaging Application....'
-    sh 'mvn package'
+    sh 'mvn pacakage'
 }
 def deployApp(){
     echo 'Deploying Application....'
     echo "Deploying version ${params.VERSION}"
+}
+
+def buildJar(){
+      echo "====++++Building Jar++++===="
+      sh 'mvn package'
+}
+
+def buildDockerImage(){
+    withCredentials([
+         usernamePassword(credentialsId:'dockerhub', usernameVariable: USER, passwordVariable: PASSWORD)]){
+         echo "====++++Building Docker Image++++===="
+         sh 'docker build -t richieoscar/chatty-app:1.0 .'
+         sh  "echo $PASSWORD | docker login - u $USER --password-stdin"
+         sh 'docker push richieoscar/chatty-app:1.0 '
 }
 
 return this
