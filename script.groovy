1668cad0d17afeff07461870ef720d14f513ba22
Oscar Anyiam <oscaranyiam94@gmail.com>
2022-04-10 17:51:19 +0100

    configured jenkins to commit and push to remote repository

--- script.groovy
+++ script.groovy
@@ -39,9 +39,9 @@ def commitVersionIncrementToGit(){
             {
                 echo "====++++Commiting To Remote Repository++++===="
                 sh "git remote set-url origin https://${USER}:${PASSWORD}@github.com/richieoscar/chatty.git "
-                sh  "git add ."
-                sh "git commit -m ci: version change"
-                sh "git push origin main"
+                sh  'git add .'
+                sh 'git commit -m "ci: version change"'
+                sh 'git push origin main'
             }
 }
 
